1a69115cd652087ffa73c0bda3e00d5c717893c8
aishwarya567 <austins.aish@gmail.com>
2020-09-03 16:15:54 +0530

    Indent anagram

--- javascript/anagram.js
+++ javascript/anagram.js
@@ -1,7 +1,6 @@
-function anagram(str1,str2){
-    return str1.split('').sort().join()===str2.split('').sort().join()
-
+function anagram(str1, str2) {
+    return str1.split('').sort().join() === str2.split('').sort().join()
 }
-console.log(anagram("listen","silent"));
-console.log(anagram("add","dad"));
-console.log(anagram("Moment","movement"));
+console.log(anagram("listen", "silent"));
+console.log(anagram("add", "dad"));
+console.log(anagram("Moment", "movement"));
